2a928d8277940a6c8aedb6bdf942271572e82bc0
patod01 <patocv2@hotmail.com>
2023-04-04 22:20:21 -0400

    added command to run container

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@
 pip install --upgrade pip
 pip install --no-cache-dir -r req.txt
 python try.py
+# docker run --rm -d --name mac -p 5001:5000 -w /app -v "$(pwd)":/app python:3.11.2-alpine3.17 ash run.sh
