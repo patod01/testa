f1d6593e9be01b1c1a952bc820d4b3712d8c767c
patod01 <patocv2@hotmail.com>
2023-04-11 09:38:32 -0400

    added run command

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
-#!/bin/ash
+#! /bin/ash
 
 pip install --upgrade pip
 pip install --no-cache-dir -r req.txt
+
 # python papp.py
 python try.py
-# docker run --rm -d --name mac -p 5001:5000 -w /app -v "$(pwd)":/app python:3.11.2-alpine3.17 ash run.sh
+
+# docker run --rm -it --name mac -p 10111:5000 -w /app -v "$(pwd)":/app python:3.11.2-alpine3.17 ash run.sh
